fa8f51a1b049b5763ee8ea5293dba124dc4282c4
Neel Raiyani <nraiyani@gmail.com>
2020-03-28 17:20:57 -0400

    Move the square around.

--- src/rogue-like.lisp
+++ src/rogue-like.lisp
@@ -3,16 +3,15 @@
 (in-package #:rogue-like)
 
 ;;- What this file exports -----------------------------------------------------
-(export '(hello-world
-	  basic-window))
+(export '(basic-window))
+ 
+;;- Basic Key Movement ---------------------------------------------------------
 
-;;- Test Package Setup ---------------------------------------------------------
-(defun hello-world ()
-  "Print 'Hello World'"
-  (print "Hello World")
-  nil)
+(defvar *square-x* 0 "X-Origin of the Square")
+(defvar *square-y* 0 "Y-Origin of the Square")
+(defvar *square-size* 100 "Width of the Square")
+(defvar *step-size* 10 "Move by units")
 
-;;- Basic Window Example -------------------------------------------------------
 (defun set-viewport (width height)
   "Create an orthographic project matrix."
   (let* ((bottom (/ height 2.0))
@@ -32,17 +31,25 @@
   (set-viewport w h))
 
 ; Callback to close window when 'ESC' is pressed
-(def-key-callback quit-on-escape (window key scancode action mod-keys)
+(def-key-callback on-key-press (window key scancode action mod-keys)
   (declare (ignore window scancode mod-keys))
-  (when (and (eq key :escape) (eq action :press))
-    (set-window-should-close)))
+  (when (eq action :press)
+    (case key
+      ((:escape) (set-window-should-close))
+      ((:w :up) (setq *square-y* (- *square-y* *step-size*)))
+      ((:s :down) (setq *square-y* (+ *square-y* *step-size*)))
+      ((:a :left) (setq *square-x* (- *square-x* *step-size*)))
+      ((:d :right) (setq *square-x* (+ *square-x* *step-size*))))))
 
 (defun render ()
   "Render a white rectangle on black background."
   (gl:clear :color-buffer)
   (gl:with-pushed-matrix
     (gl:color 1 1 1)
-    (gl:rect -25 -25 25 25)))
+    (gl:rect (- *square-x* (/ *square-size* 2.0))
+	     (- *square-y* (/ *square-size* 2.0))
+	     (+ *square-x* (/ *square-size* 2.0))
+	     (+ *square-y* (/ *square-size* 2.0)))))
 
 (defun basic-window (width height)
   "Create basic window and show it."
@@ -51,7 +58,7 @@
 		       :width width
 		       :height height)
       (setf %gl:*gl-get-proc-address* #'get-proc-address)
-      (set-key-callback 'quit-on-escape)
+      (set-key-callback 'on-key-press)
       (set-window-size-callback 'update-viewport)
       (gl:clear-color 0 0 0 0)
       (set-viewport width height)
